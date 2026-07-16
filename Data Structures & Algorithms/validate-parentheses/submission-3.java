6cec57a2991dfdefa68d3ad9ad8d5f0a5eef77fb
Sumit Verma <149126679+Sumit191105@users.noreply.github.com>
2026-07-16 22:56:24 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c:s.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()) return false;
+                if(c==')' && stack.peek()=='(') stack.pop();
+                else if(c=='}' && stack.peek()=='{') stack.pop();
+                else if(c==']' && stack.peek()=='[') stack.pop();
+                else return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
