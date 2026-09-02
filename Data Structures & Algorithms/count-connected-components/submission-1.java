5b48b92d3369c15d4ad620796fd052500a6965d8
Sumit Verma <149126679+Sumit191105@users.noreply.github.com>
2026-09-02 09:58:50 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.java
@@ -0,0 +1,65 @@
+class Solution {
+   /* private void dfs(ArrayList<ArrayList<Integer>> adj,boolean[] visited,int node){
+        visited[node] = true;
+        for(int it:adj.get(node)){
+            if(!visited[it]){
+                dfs(adj,visited,it);
+            }
+        }
+    }
+    public int countComponents(int n, int[][] edges) {
+        int m = edges.length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i =0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i =0;i<m;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        boolean[] visited = new boolean[n];
+        int components =0;
+        for(int i =0;i<n;i++){
+            if(!visited[i]){
+                components++;
+                dfs(adj,visited,i);
+            }
+        }
+        return components;
+    }
+    */
+    public int countComponents(int n, int[][] edges){
+        int m = edges.length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i =0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        boolean[] visited = new boolean[n];
+        int components =0;
+        for(int i =0;i<n;i++){
+            if(!visited[i]){
+                components++;
+                Queue<Integer> q = new LinkedList<>();
+                q.add(i);
+                visited[i]=true;
+                while(!q.isEmpty()){
+                    int node = q.poll();
+                    for(int it : adj.get(node)){
+                        if(!visited[it]){
+                            visited[it]=true;
+                            q.add(it);
+                        }
+                    }
+                }
+            }
+        }
+       
+        return components;
+    }
+}
