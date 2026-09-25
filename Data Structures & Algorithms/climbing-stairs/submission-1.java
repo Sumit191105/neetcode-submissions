83ab8e26a7f13663ab7bd7542fca54f1312b549a
Sumit Verma <149126679+Sumit191105@users.noreply.github.com>
2026-09-25 14:05:00 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    private int solve(int n, int[] dp){
+        if(n==1 || n==2) return n;
+        if(dp[n]!=0) return dp[n];
+        dp[n] = solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
+
+    public int climbStairs(int n) {
+       int dp[] = new int[n+1];
+       return solve(n,dp);
+    }
+}
